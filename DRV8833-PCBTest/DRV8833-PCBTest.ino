65f2c2fc9b24a093be87cde6dc32a5c72b0251cd
Tingliang Zhang <zhangtl16@mails.tsinghua.edu.cn>
2020-04-05 17:41:22 +0800

    Create DRV8833-PCBTest.ino

---
+++ DRV8833-PCBTest/DRV8833-PCBTest.ino
@@ -0,0 +1,28 @@
+/*Example sketch to control a stepper motor with A4988/DRV8825 stepper motor driver and Arduino without a library. More info: https://www.makerguides.com */
+
+// Define stepper motor connections and steps per revolution:
+#define dirPin 7
+#define stepPin 6
+#define stepsPerRevolution 200
+
+void setup() {
+  // Declare pins as output:
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+}
+
+void loop() {
+  // Set the spinning direction clockwise:
+  digitalWrite(dirPin, HIGH);
+
+  //Spin the stepper motor 5 revolutions fast:
+  for (int i = 0; i < 5 * stepsPerRevolution; i++) {
+    // These four lines result in 1 step:
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(500);
+  }
+
+  delay(1000);
+}
