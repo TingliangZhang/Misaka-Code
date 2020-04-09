7d5dedcab405e21ce1ec27a453b8c7a8a54c6d65
Tingliang Zhang <zhangtl16@mails.tsinghua.edu.cn>
2020-04-09 22:12:45 +0800

    Forward

--- Stepper-3-Movement/Stepper-3-Movement.ino
+++ Stepper-3-Movement/Stepper-3-Movement.ino
@@ -8,20 +8,22 @@ AccelStepper stepper1(AccelStepper::DRIVER, 6, 7);
 AccelStepper stepper2(AccelStepper::DRIVER, 8, 9);
 AccelStepper stepper3(AccelStepper::DRIVER, 10, 11);
 
+float MaxSpeed = 1200.0;
+
 // Unit : Step or Step per second
 // If one step is 1.8 degree, one round is 360/1.8 = 200 step. 
 
 void setup() {
   // put your setup code here, to run once:
-    stepper1.setMaxSpeed(1200.0);
+    stepper1.setMaxSpeed(MaxSpeed);
     stepper1.setAcceleration(300.0);
     stepper1.moveTo(1000000);
     
-    stepper2.setMaxSpeed(1200.0);
+    stepper2.setMaxSpeed(MaxSpeed);
     stepper2.setAcceleration(300.0);
     stepper2.moveTo(1000000);
     
-    stepper3.setMaxSpeed(1200.0);
+    stepper3.setMaxSpeed(MaxSpeed);
     stepper3.setAcceleration(300.0);
     stepper3.moveTo(1000000); 
 }
