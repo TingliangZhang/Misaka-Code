97640006ec9dc86052310526036b00b6d2121c53
Tingliang Zhang <zhangtl16@mails.tsinghua.edu.cn>
2020-04-20 16:15:16 +0800

    Update MisakaCarV1.ino

--- MisakaCarV1/MisakaCarV1.ino
+++ MisakaCarV1/MisakaCarV1.ino
@@ -1,9 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
+/*Example sketch to control a stepper motor with A4988/DRV8825 stepper motor driver and Arduino without a library. More info: https://www.makerguides.com */
 
+// Using DRV8825
+// D6-D12 PWM Pin
+// D6 : the Step input to the driver. Low to high transition means to step  
+// D7 : the Direction input the driver. High means forward.
+
+// Unit : Step or Step per second
+// If one step is 1.8 degree, one round is 360/1.8 = 200 step. 
+
+
+
+// Define stepper motor connections and steps per revolution:
+#define dirPin 7
+#define stepPin 6
+#define stepsPerRevolution 200
+
+void setup() {
+  // Declare pins as output:
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Set the spinning direction clockwise:
+  digitalWrite(dirPin, HIGH);
 
+  //Spin the stepper motor 5 revolutions fast:
+  for (int i = 0; i < 5 * stepsPerRevolution; i++) {
+    // These four lines result in 1 step:
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(500);
+  }
+
+  delay(1000);
 }
