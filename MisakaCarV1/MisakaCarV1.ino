eb1e61df17865fd9933c2e3274be2da182404a4f
Tingliang Zhang <zhangtl16@mails.tsinghua.edu.cn>
2020-04-27 10:37:32 +0800

    XBee

--- MisakaCarV1/MisakaCarV1.ino
+++ MisakaCarV1/MisakaCarV1.ino
@@ -1,3 +1,19 @@
+#include <XBee.h>
+
+/*
+https://github.com/andrewrapp/xbee-arduino/blob/wiki/DevelopersGuide.md
+This example is for Series 2 XBee
+Receives a ZB RX packet and sets a PWM value based on packet data.
+Error led is flashed if an unexpected packet is received
+*/
+
+XBee xbee = XBee();
+XBeeResponse response = XBeeResponse();
+// create reusable response objects for responses we expect to handle 
+ZBRxResponse rx = ZBRxResponse();
+ModemStatusResponse msr = ModemStatusResponse();
+
+
 /*Example sketch to control a stepper motor with A4988/DRV8825 stepper motor driver and Arduino without a library. More info: https://www.makerguides.com */
 
 // Using DRV8825
@@ -22,6 +38,23 @@
 #define TimeScale 16
 
 #define stepsPerRevolution 200
+
+int statusLed = 13;
+int errorLed = 13;
+int dataLed = 13;
+
+void flashLed(int pin, int times, int wait) {
+    
+    for (int i = 0; i < times; i++) {
+      digitalWrite(pin, HIGH);
+      delay(wait);
+      digitalWrite(pin, LOW);
+      
+      if (i + 1 < times) {
+        delay(wait);
+      }
+    }
+}
 
 float speedabs = 0;
 float stepsabs = 0;
@@ -112,6 +145,10 @@ void Run()
 
 
 void setup() {
+  pinMode(statusLed, OUTPUT);
+  pinMode(errorLed, OUTPUT);
+  pinMode(dataLed,  OUTPUT);
+  
   // Declare pins as output:
   pinMode(stepPin1, OUTPUT);
   pinMode(dirPin1, OUTPUT);
@@ -119,7 +156,12 @@ void setup() {
   pinMode(dirPin2, OUTPUT);
   pinMode(stepPin3, OUTPUT);
   pinMode(dirPin3, OUTPUT);
-  Forward(5);
+
+  // start serial
+  Serial1.begin(9600);
+  xbee.begin(Serial1);
+  
+//  Forward(5);
 }
 
 void loop() {
@@ -128,4 +170,50 @@ void loop() {
 //  SetSpeed(dirPin3, stepPin3, 100);
 //
 //  Run();
+
+  // continuously reads packets, looking for ZB Receive or Modem Status
+  xbee.readPacket();
+  
+  if (xbee.getResponse().isAvailable()) {
+    // got something
+    
+    if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
+      // got a zb rx packet
+      
+      // now fill our zb rx class
+      xbee.getResponse().getZBRxResponse(rx);
+
+      Forward(rx.getData());
+          
+//      if (rx.getOption() == ZB_PACKET_ACKNOWLEDGED) {
+//          // the sender got an ACK
+//          flashLed(statusLed, 10, 10);
+//      } else {
+//          // we got it (obviously) but sender didn't get an ACK
+//          flashLed(errorLed, 2, 20);
+//      }
+//      // set dataLed PWM to value of the first byte in the data
+//      analogWrite(dataLed, rx.getData(0));
+    } else if (xbee.getResponse().getApiId() == MODEM_STATUS_RESPONSE) {
+      xbee.getResponse().getModemStatusResponse(msr);
+      // the local XBee sends this response on certain events, like association/dissociation
+      
+      if (msr.getStatus() == ASSOCIATED) {
+        // yay this is great.  flash led
+        flashLed(statusLed, 10, 10);
+      } else if (msr.getStatus() == DISASSOCIATED) {
+        // this is awful.. flash led to show our discontent
+        flashLed(errorLed, 10, 10);
+      } else {
+        // another status
+        flashLed(statusLed, 5, 10);
+      }
+    } else {
+      // not something we were expecting
+      flashLed(errorLed, 1, 25);    
+    }
+  } else if (xbee.getResponse().isError()) {
+    //nss.print("Error reading packet.  Error code: ");  
+    //nss.println(xbee.getResponse().getErrorCode());
+  }
 }
--- MisakaCarV1/REF/Series2_Rx/Series2_Rx.ino
+++ MisakaCarV1/REF/Series2_Rx/Series2_Rx.ino
@@ -39,11 +39,11 @@ void flashLed(int pin, int times, int wait) {
     
     for (int i = 0; i < times; i++) {
       digitalWrite(pin, HIGH);
-      delay(wait);
+      delay(wait/16);
       digitalWrite(pin, LOW);
       
       if (i + 1 < times) {
-        delay(wait);
+        delay(wait/16);
       }
     }
 }
