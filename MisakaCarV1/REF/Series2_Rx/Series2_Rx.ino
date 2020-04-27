e425ff0cbde329c787249c887fcaba2ab7c0dab9
Tingliang Zhang <zhangtl16@mails.tsinghua.edu.cn>
2020-04-27 10:42:24 +0800

    Update Series2_Rx.ino

--- MisakaCarV1/REF/Series2_Rx/Series2_Rx.ino
+++ MisakaCarV1/REF/Series2_Rx/Series2_Rx.ino
@@ -39,11 +39,11 @@ void flashLed(int pin, int times, int wait) {
     
     for (int i = 0; i < times; i++) {
       digitalWrite(pin, HIGH);
-      delay(wait/16);
+      delay(wait);
       digitalWrite(pin, LOW);
       
       if (i + 1 < times) {
-        delay(wait/16);
+        delay(wait);
       }
     }
 }
